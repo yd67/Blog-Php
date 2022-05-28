5e68fc1ac90ca732a5a0d77847171118491c4f99
Darius <yvens67@hotmail.fr>
2022-05-28 21:29:42 +0200

    mise en place de Ckeditor

--- src/Controller/Admin/AdminPostController.php
+++ src/Controller/Admin/AdminPostController.php
@@ -17,6 +17,18 @@ class AdminPostController extends BaseAdminController
     {
         if( !empty($_POST)){
 
+              // treatment of images
+            //   if (!empty($file)) {
+            //     $extention = explode('.',$file['name']) ;
+            //     $extention = $extention[1];
+            //     $tmp_name = $file['tmp_name'] ;
+                
+            //     $imgName = 'profil-'.time().'.'.$extention ;
+            //     $path_dest = ROOT.'/public/upload/post/'.$imgName ;
+
+            //     move_uploaded_file($tmp_name,$path_dest);
+            // }
+
             var_dump($_POST);
         }
 
